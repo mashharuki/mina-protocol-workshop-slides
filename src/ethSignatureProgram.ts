09d49322499bf9bbb6a242cdeffcb2670fbc4a25
Coby <coby@o1labs.org>
2024-10-24 15:51:49 -0400

    create the zk program

---
+++ src/ethSignatureProgram.ts
@@ -0,0 +1,26 @@
+import {
+  Bool,
+  Bytes,
+  createEcdsa,
+  createForeignCurve,
+  ZkProgram,
+  Crypto,
+} from 'o1js';
+
+class Secp256k1 extends createForeignCurve(Crypto.CurveParams.Secp256k1) {}
+class ECDSA extends createEcdsa(Secp256k1) {}
+class Bytes32 extends Bytes(32) {}
+
+const EthSignatureProgram = ZkProgram({
+  name: 'EthSignatureProgram',
+  publicInput: Bytes32,
+  publicOutput: Bool,
+  methods: {
+    verifySignature: {
+      privateInputs: [ECDSA, Secp256k1],
+      async method(message: Bytes32, signature: ECDSA, publicKey: Secp256k1) {
+        return { publicOutput: signature.verifyEthers(message, publicKey) };
+      },
+    },
+  },
+});
